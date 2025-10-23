54f426741dfb388c2cd5cb186084aaeb9dc7b03e
Lewis Mbae <lewis@coderabbit.ai>
2025-10-22 17:12:49 -0700

    Update train.py

--- train.py
+++ train.py
@@ -325,8 +325,8 @@ while True:
             mfu = raw_model.estimate_mfu(batch_size * gradient_accumulation_steps, dt)
             running_mfu = mfu if running_mfu == -1.0 else 0.9*running_mfu + 0.1*mfu
         print(f"iter {iter_num}: loss {lossf:.4f}, time {dt*1000:.2f}ms, mfu {running_mfu*100:.2f}%")
-    iter_num += 1
-    local_iter_num += 1
+    # iter_num += 1
+    # local_iter_num += 1
 
     # termination conditions
     if iter_num > max_iters:
